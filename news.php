60a1bb5299ecf46b97ea17f03ad81842154c2291
javaIntroAkshay <agiri557@student.fuhsd.org>
2024-12-02 21:10:54 -0800

    news sort

--- news.php
+++ news.php
@@ -4,47 +4,43 @@ $pagedescription = 'Latest updates';
 include('includes/header.php');
 ?>
 
- <?php
-    $news_data = file_get_contents('content/news.json');
-    $news = json_decode($news_data, true);
+<?php
+$news_data = file_get_contents('content/news.json');
+$news = json_decode($news_data, true);
 
-    $total = count($news);
+// Sort news posts by date (most recent to oldest)
+usort($news, function($a, $b) {
+    $dateA = strtotime($a['year'] . '-' . $a['month'] . '-' . $a['date']);
+    $dateB = strtotime($b['year'] . '-' . $b['month'] . '-' . $b['date']);
+    return $dateB - $dateA; // Most recent dates first
+});
 
-    $post = $news[0];
+$total = count($news);
+
+// Display the first post
+$post = $news[0];
+echo '<div class="box-gray news">';
+echo '    <h2>' . $post['title'] . '</h2>';
+echo '    <h4>' . $post['month'] . '/' . $post['date'] . '/' . $post['year'] . '</h4>';
+echo $post['content'];
+echo '</div>';
+
+?>
+
+<div class="cols-wrapper">
+
+<?php
+// Display the remaining posts
+for ($i = 1; $i < $total; $i++) {
+    $post = $news[$i];
     echo '<div class="box-gray news">';
     echo '    <h2>' . $post['title'] . '</h2>';
     echo '    <h4>' . $post['month'] . '/' . $post['date'] . '/' . $post['year'] . '</h4>';
     echo $post['content'];
     echo '</div>';
-    ?>
+}
+?>
 
-    <div class="cols-wrapper">
-
-    <?php
-
-    // idk
-
-    for ($i=1; $i<$total; $i++) {
-    	$post = $news[$i];
-        echo '<div class="box-gray news">';
-        echo '    <h2>'. $post['title'] . '</h2>';
-        echo '    <h4>' . $post['month'] . '/' . $post['date'] . '/' . $post['year'] . '</h4>';
-        echo $post['content'];
-        echo '</div>';
-        $i++;
-    }
-
-    for ($i=2; $i<$total; $i++) {
-    	$post = $news[$i];
-        echo '<div class="box-gray news">';
-        echo '    <h2>'. $post['title'] . '</h2>';
-        echo '    <h4>' . $post['month'] . '/' . $post['date'] . '/' . $post['year'] . '</h4>';
-        echo $post['content'];
-        echo '</div>';
-        $i++;
-    }
-
-    ?>
 </div>
 
 <?php include('includes/footer.php'); ?>
